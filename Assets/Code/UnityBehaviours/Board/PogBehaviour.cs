b897be0f917bb9ee4a2f76bba3f6c3e1acdaf57b
AsherLaakes <asherl@live.ca>
2015-11-01 18:51:26 -0500

    pogs load images from urls

---
+++ Assets/Code/UnityBehaviours/Board/PogBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Code.UnityBehaviours.Board
+{
+    public class PogBehaviour : MonoBehaviour
+    {
+        public MeshRenderer Mesh;
+
+        public void Start()
+        {
+            var targetUrl = "https://upload.wikimedia.org/wikipedia/commons/b/b0/PSM_V37_D105_English_tabby_cat.jpg";
+
+            StartCoroutine(SetImageToUrl(targetUrl));
+        }
+
+        IEnumerator SetImageToUrl(string url)
+        {
+            var www = new WWW(url);
+            
+            yield return www;
+            
+            Mesh.material.mainTexture = www.texture;
+        }
+    }
+}
--- Assets/Code/UnityBehaviours/UnityReferenceMaster.cs
+++ Assets/Code/UnityBehaviours/UnityReferenceMaster.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Assets.Code.DataPipeline;
 using Assets.Code.DataPipeline.Providers;
